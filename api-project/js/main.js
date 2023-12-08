797f16022735450f8d4c9425c6afa3f102f8c6ef
js1247 <jasonshi1247@gmail.com>
2023-12-08 09:23:20 -0500

    923

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,16 +1,15 @@
 import "../css/style.css";
+//get api from hypixel skyblock
+const URL = "https://api.hypixel.net/v2/skyblock/auctions"; 
 
-const URL = "//https://api.hypixel.net/"; 
-//still need to get a api from hypixel
 async function getData(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
-    console.log(data);
+    data.auctions.forEach(data => console.log(data));
   }
   catch (error) {
     console.log(error);
   }
 }
-
 getData(URL);
