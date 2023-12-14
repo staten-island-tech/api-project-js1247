8f4d73705ad5fdc030b5522d9fb5a9f629d507f1
js1247 <jasonshi1247@gmail.com>
2023-12-14 09:25:06 -0500

    Create loops.js

---
+++ api-project/loops.js
@@ -0,0 +1,43 @@
+const dennis = {
+    name: "Dennis", //String
+    age: 16, //Integer
+};
+let x = Array.from(dennis.name);
+console.log(x);
+for(let i = 0; i< dennis.name.length; i++) {
+    console.log(dennis.name[i]);
+}
+let i = 0;
+while(i < dennis.name.length) {
+    console.log(dennis.name[i]);
+    i++;
+}
+console.log(12 + Numbber('13'));
+console.log(dennis.name);
+
+function needle(name, search) {
+    if(name.includes(search)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(needle(dennis.name, "D"));
+
+function sortString(name){
+    let x = [...name].sort();
+    console.log(x);
+}
+sortString("sadwasdwasdddddddddddddddd")
+let x = [1, 2, 3];
+let y = [4, 5, 6];
+let z = {...x, ...y};
+x = [...x, ...y];
+console.log(z);
+
+function removeL(name) {
+    let x = name;
+    x.splice(-1);
+    console.log(x);
+}
+removeL(dennis.name);
