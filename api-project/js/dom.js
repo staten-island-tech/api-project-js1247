8949cfac1eba14d700caf8c3a52521a2fa7cdaf3
js1247 <jasonshi1247@gmail.com>
2024-01-01 15:31:30 -0500

    Update dom.js

--- api-project/js/dom.js
+++ api-project/js/dom.js
@@ -1,4 +1,8 @@
 export { DOMSelectors };
 const DOMSelectors = {
     content: document.querySelector(".content"),
-}
+    searchForm: document.querySelector("#searchForm"),
+    search: document.querySelector(".search"),
+    searchBtn: document.querySelector(".search-btn"),
+    allBtn: document.querySelector(".all-btn"),
+};
