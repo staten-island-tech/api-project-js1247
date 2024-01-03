52e512895d6036b9b4b4ff339b85bbdaeb7b0e8c
js1247 <jasonshi1247@gmail.com>
2024-01-02 22:47:46 -0500

    1047

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,20 +1,15 @@
 import "../css/style.css";
 import { DOMSelectors } from "./dom";
-const URL = "https://api.hypixel.net/v2/skyblock/auctions"; 
+const URL = "https://api.hypixel.net/v2/resources/skyblock/items"; 
 
 async function getData(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
-    data.auctions.forEach(data => console.log(data));
+    data.items.forEach(data => console.log(data));
   }
   catch (error) {
     console.log(error);
   }
-  async function insertAuction() {
-  }
-  insertAuction();
-  console.log("");
-  document.querySelector(".cards").textContent = "";
 };
 getData(URL);
